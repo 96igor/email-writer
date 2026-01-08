a6107039b2e31627458fec4e7e71f37c6f918a4d
Игорь Царёв <tsarov88@gmail.com>
2026-01-08 18:07:57 +0200

    Build the prompt

---
+++ src/main/java/com/email/writer/app/EmailGeneratorService.java
@@ -0,0 +1,25 @@
+package com.email.writer.app;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailGeneratorService {
+
+    public String generateEmailReply(EmailRequest emailRequest) {
+        // Build the prompt
+        String prompt = buildPrompt(emailRequest);
+        // Cruft a request
+        // Do request and get response
+        // Return response
+    }
+
+    private String buildPrompt(EmailRequest emailRequest) {
+        StringBuilder prompt = new StringBuilder();
+        prompt.append("Generate a professional email reply for the following email content. Please don't generate a subject line");
+        if (emailRequest.getTone() != null && !emailRequest.getTone().isEmpty()) {
+            prompt.append("Use a ").append(emailRequest.getTone()).append(" tone.");
+        }
+        prompt.append("\nOriginal email: \n").append(emailRequest.getEmailContent());
+        return prompt.toString();
+    }
+}
